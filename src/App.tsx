d63ac4453da68382b10cd9e7d1c5e36b69640dc2
MaxOF <crokster1996@gmail.com>
2022-07-02 14:37:04 +0300

    minimal structure for add functional components

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,54 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import s from './Main.module.scss'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const optionArr: string[] = ['all', 'art', 'biography', 'computers', 'history', ' medical', 'poetry']
+
+    return (
+        <div className="App">
+            <main className={s.page}>
+                <section className={s.page__search}>
+                    <div className={`${s.search__container} _container`}>
+                        <div className={s.search__body}>
+                            <h1 className={s.search__title}>Search for books</h1>
+                            <form className={s.search__form}>
+                                <div className={s.searchBlock__input}>
+                                    <input type="text"/>
+                                </div>
+                                <div className={s.select__category}>
+                                    <select name="categories" id="" defaultValue='all'>
+                                        {
+                                            optionArr.map(opt => {
+                                                return <option value={opt}>{opt}</option>
+                                            })
+                                        }
+                                    </select>
+                                </div>
+                                <div className={s.select__sort}>
+                                    <select name="sorting" id="" defaultValue='relevance'>
+                                        <option value='relevance'>relevance</option>
+                                        <option value='newest'>newest</option>
+                                    </select>
+                                </div>
+                                <div className={s.search__button}>
+                                    <button>Search</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </section>
+                <div className={s.page__results}>
+                    <div>
+                        search results
+                    </div>
+
+                </div>
+            </main>
+
+        </div>
+    );
 }
 
 export default App;
